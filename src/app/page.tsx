42d983f8cb7e8e30f83b8299172f8800800eff0f
Shaikh Kamil <shaikhkamil789@gmail.com>
2026-08-04 00:55:54 +0530

    Divide the pizza and burgers in two categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,22 @@ export default async function Home() {
   const items = await getItems()
   return (
     <div>
-      <div className="text-center pt-8 font-extrabold text-2xl">
+      <div className="text-center pt-8 font-extrabold text-2xl text-black">
         <h1>RaftLab's Special Canteen</h1>
       </div>
-      <div className="flex mx-30 my-20 font-sans flex-wrap gap-10">
+      {/* Burgers  */}
+        <h3 className="text-center mt-15 font-extrabold text-xl text-black">Burgers🍔</h3>
+      <div className="flex mx-30 mb-20 mt-10 font-sans flex-wrap gap-10 justify-center items-center">
         {items.map((item: Item) => (
+          item.type == "burger" &&
+          <Card items={item} key={item._id} />
+        ))}
+      </div>
+      {/* Pizzas  */}
+      <h3 className="text-center mt-15 font-extrabold text-xl text-black">Pizza🍕</h3>
+      <div className="flex mx-30 mb-20 mt-10 font-sans flex-wrap gap-10 justify-center items-center">
+        {items.map((item: Item) => (
+          item.type == "pizza" &&
           <Card items={item} key={item._id} />
         ))}
       </div>
--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import { Btn } from "@/components/common/Button";
 import { Item } from "@/types/item.type";
@@ -19,13 +19,13 @@ export const Card = ({ items }: { items: Item }) => {
         }
     }
     return (
-        <div className="w-80 h-100 border-2 border-[#FF6A3D] rounded-md">
-            <Image className="h-50 w-80" height={1900} width={1267} quality={100} alt="image" src={items.image} loading="eager" />
+        <div className="w-80 h-100 rounded-md border-4 border-black">
+            <Image className="h-50 w-80" height={1900} width={1267} quality={100} alt="image" src={items.image} loading="lazy" />
             <h1 className="py-5 text-center font-bold capitalize">
-                {items.name}
+                {items.name} {items.is_veg ? "🟢" : "🔴"}
             </h1>
-            <p className="text-black px-5">{items.description}</p>
-            <div className="ml-60">
+            <p className="text-black px-5 text-ellipsis h-20">{items.description}</p>
+            <div className="sm:ml-60 ml-55">
                 <Btn name="Buy" isLoading={loading} onClick={() => handleClick()} />
             </div>
         </div>
